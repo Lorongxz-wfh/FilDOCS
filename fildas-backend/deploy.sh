fce2a8eb55e9f0de60354964d34c328a91532cc9
Lorongxz <lorongxz.wfh@gmail.com>
2026-03-13 15:04:15 +0800

    Fix: use custom entrypoint for migrations

--- fildas-backend/deploy.sh
+++ fildas-backend/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
-set -e
 
 cd /var/www/html
 
-php artisan config:clear || true
-php artisan config:cache || true
+php artisan config:clear
+php artisan config:cache
 php artisan storage:link --force || true
-php artisan migrate:fresh --force --seed || true
+php artisan migrate:fresh --force --seed
+
+# Hand off to the base image's start script
+exec /start.sh
