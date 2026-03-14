6010ac7a8d7f375689df4e004eb7ce047202dc1b
Lorongxz <lorongxz.wfh@gmail.com>
2026-03-14 11:07:45 +0800

    feat: add Vercel analytics

--- fildas-frontend/src/main.tsx
+++ fildas-frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./lib/ThemeContext";
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </BrowserRouter>,
 );
